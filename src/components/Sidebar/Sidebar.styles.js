efadf7584bb30ce2f2b89deaa4bed883a80cb9d0
IP-LJH <lee-junghoon@samsong.com>
2021-10-01 19:01:01 +0900

    jsx파일 js로 변경

---
+++ src/components/Sidebar/Sidebar.styles.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled"
+
+export const SidebarContainer = styled.div`
+    width: 20%;
+    max-width: 280px;
+    min-width: 80px;
+    background-color: #90d5ec;
+    background-image: linear-gradient(315deg, #90d5ec 0%, #fc575e 74%);   
+`
